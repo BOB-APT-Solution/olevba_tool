0de36b739e1680d4a6776430d16b6ae2dd3104cb
juyoung <67878157+wa1575@users.noreply.github.com>
2020-07-29 16:43:11 +0900

    Create oleole.py
    olevba_base tool

---
+++ oleole.py
@@ -0,0 +1,57 @@
+import sys
+import os
+from oletools.olevba import VBA_Parser, TYPE_OLE, TYPE_OpenXML, TYPE_Word2003_XML, TYPE_MHTML, detect_autoexec, VBA_Scanner
+
+#PYTHON 2,3버전 모두 호환 
+if sys.version_info[0]<= 2:
+      #python 2
+      from oletools.olevba import VBA_Parser
+else:
+     #python 3
+	  from oletools.olevba3 import VBA_Parser
+
+# VBA 퍼져 온 
+vbaparser = VBA_Parser(sys.argv[1])
+  # 매크로 검사
+if not vbaparser.detect_vba_macros():
+ print ('error', "매크로 파일이 감지되지 않습니다.")
+ #경고모듈 넣는 위치 
+ sys.exit(1)
+
+print ('info', "매크로가 감지됩니다!")
+
+for (filename, stream_path, vba_filename, vba_code) in vbaparser.extract_macros():
+  print ("- "*79)
+  print ("파일이름    :", filename)
+  print ("OLE stream  :", stream_path)
+  print ("VBA filename:", vba_filename)
+  print ("- "*39)
+  print ("vba_code",vba_code)
+
+     
+vba_scanner = VBA_Scanner(vba_code)
+results = vba_scanner.scan(include_decoded_strings=True)
+for kw_type, keyword, description in results:
+ print ("type=%s - keyword=%s - description=%s" % (kw_type, keyword, description))
+     
+autoexec_keywords = detect_autoexec(vba_code)
+if autoexec_keywords:
+ print("Auto-executable Macro keyword가 감지됩니다. :")
+ for keyword, description in autoexec_keywords:
+   print("%s: %s' % (keyword, description)")
+else:
+ print ("Auto-executable Macro keywords가 감지되지 않습니다.")
+
+results = vbaparser.analyze_macros()
+for kw_type, keyword, description in results:
+ print ("type=%s - keyword=%s - description=%s" % (kw_type, keyword, description))
+ print ("AutoExec keywords: %d" % vbaparser.nb_autoexec)
+ print ("Suspicious keywords: %d" % vbaparser.nb_suspicious)
+ print ("IOCs: %d" % vbaparser.nb_iocs)
+ print ("Hex obfuscated strings: %d" % vbaparser.nb_hexstrings)
+ print ("Base64 obfuscated strings: %d" % vbaparser.nb_base64strings)
+ print ("Dridex obfuscated strings: %d" % vbaparser.nb_dridexstrings)
+ print ("VBA obfuscated strings: %d" % vbaparser.nb_vbastrings)
+
+  # 퍼져 닫기 
+vbaparser.close()
