f2b1645118139a460af2a3f7711e0f8d484da3ec
juyoung <67878157+wa1575@users.noreply.github.com>
2020-08-18 21:33:38 +0900

    Update oleole.py

--- oleole.py
+++ oleole.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*- 
+
 import sys
 import os
 from oletools.olevba import VBA_Parser, TYPE_OLE, TYPE_OpenXML, TYPE_Word2003_XML, TYPE_MHTML, detect_autoexec, VBA_Scanner
@@ -15,15 +16,15 @@ else:
 vbaparser = VBA_Parser(sys.argv[1])
   # 매크로 검사
 if not vbaparser.detect_vba_macros():
- print ('error', "매크로 파일이 감지되지 않습니다.")
+ print ('error', "macro not detected ")
  #경고모듈 넣는 위치 
  sys.exit(1)
 
-print ('info', "매크로가 감지됩니다!")
+print ('info', "macro detected ! ")
 
 for (filename, stream_path, vba_filename, vba_code) in vbaparser.extract_macros():
   print ("- "*79)
-  print ("파일이름    :", filename)
+  print ("filename    :", filename)
   print ("OLE stream  :", stream_path)
   print ("VBA filename:", vba_filename)
   print ("- "*39)
@@ -37,11 +38,11 @@ for kw_type, keyword, description in results:
      
 autoexec_keywords = detect_autoexec(vba_code)
 if autoexec_keywords:
- print("Auto-executable Macro keyword가 감지됩니다. :")
- for keyword, description in autoexec_keywords:
+ print("Auto-executable Macro keyword is detected. :")
+for keyword, description in autoexec_keywords:
    print("%s: %s' % (keyword, description)")
 else:
- print ("Auto-executable Macro keywords가 감지되지 않습니다.")
+ print ("Auto-executable Macro keywords is not detected.")
 
 results = vbaparser.analyze_macros()
 for kw_type, keyword, description in results:
